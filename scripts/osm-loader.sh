0d1efe9ca32e3dcadf529696815b82dcd2e54d1d
Tony Laidig <laidig@gmail.com>
2017-04-21 13:45:56 -0400

    wget > curl?

--- scripts/osm-loader.sh
+++ scripts/osm-loader.sh
@@ -7,7 +7,7 @@ mkdir -p $DATA/openstreetmap
 cd $DATA/openstreetmap
 
 # Download osm data
-curl -sS -O https://s3.amazonaws.com/mapzen.odes/ex_qsCMhLYfJTYDxvhBhwdPkwAYrnWBX.osm.pbf 
+wget -nv  https://s3.amazonaws.com/mapzen.odes/ex_qsCMhLYfJTYDxvhBhwdPkwAYrnWBX.osm.pbf 
 
 echo '##### Loaded OSM data'
 echo 'OK' >> /tmp/loadresults
