41f2914699a1bc58dedd5365fa1c328adfdb6ecc
Tony Laidig <laidig@gmail.com>
2017-04-25 17:24:51 -0400

    quiet!

--- scripts/oa-loader.sh
+++ scripts/oa-loader.sh
@@ -7,7 +7,7 @@ mkdir -p $DATA/openaddresses
 cd $DATA/openaddresses
 
 # Download New York' entries from OpenAddresses
-curl -S http://results.openaddresses.io/state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O -sS {}
+curl -sS http://results.openaddresses.io/state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O -sS {}
 ls *.zip | xargs -n 1 unzip -o
 rm *.zip README.*
 
