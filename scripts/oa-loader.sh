745e1bdf426f1bc1dd9e74c63f35b3414c4bd22a
Your Name <you@example.com>
2017-05-19 19:59:40 +0000

    more data

--- scripts/oa-loader.sh
+++ scripts/oa-loader.sh
@@ -6,8 +6,10 @@ set -e
 mkdir -p $DATA/openaddresses
 cd $DATA/openaddresses
 
-# Download New York' entries from OpenAddresses
-curl -sS http://results.openaddresses.io/state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
+# Download New York and New Jersey entries from OpenAddresses
+curl -sS -O http://results.openaddresses.io/state.txt 
+cat state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
+cat state.txt | sed 's/\s\+/\n/g' | grep 'us/nj/statewide' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
 ls *.zip | xargs -n 1 unzip -o
 rm *.zip README.*
 
--- scripts/osm-loader.sh
+++ scripts/osm-loader.sh
@@ -6,7 +6,7 @@ set -e
 cd $DATA/openstreetmap
 
 # Download osm data
-wget -q -nv -O osm.pbf  https://s3.amazonaws.com/mapzen.odes/ex_qsCMhLYfJTYDxvhBhwdPkwAYrnWBX.osm.pbf 
+wget -q -nv -O osm.pbf https://s3.amazonaws.com/mapzen.odes/ex_DJwv1Zb9GdMA6yJPb1XJE7szYDQa.osm.pbf 
 
 echo '##### Loaded OSM data'
 echo 'OK' >> /tmp/loadresults
