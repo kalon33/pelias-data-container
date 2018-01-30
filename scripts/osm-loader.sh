4e18e0eae88a0064da64dc547e836c054349eabe
Nicolas Derive <kalon33@ubuntu.com>
2018-01-30 11:39:42 +0100

    Update osm-loader.sh

--- scripts/osm-loader.sh
+++ scripts/osm-loader.sh
@@ -6,7 +6,7 @@ set -e
 cd $DATA/openstreetmap
 
 # Download osm data
-wget -q -nv -O osm.pbf https://s3.amazonaws.com/mapzen.odes/ex_DJwv1Zb9GdMA6yJPb1XJE7szYDQa.osm.pbf 
+wget -q -nv -O france-latest.osm.pbf http://download.geofabrik.de/europe/france-latest.osm.pbf
 #wget -q -nv -O osm.pbf https://s3.amazonaws.com/metro-extracts.mapzen.com/new-york_new-york.osm.pbf
 #wget -q -nv -O osm.pbf https://s3.amazonaws.com/mapzen.odes/ex_CGsZ1bjMkKL3gnqx5DjF3iM6qssMB.osm.pbf
 
