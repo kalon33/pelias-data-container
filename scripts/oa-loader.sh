20b856115ea3a8030ff4e6719a7161074dd5fe0e
Nicolas Derive <kalon33@ubuntu.com>
2018-01-30 11:38:53 +0100

    Update oa-loader.sh

--- scripts/oa-loader.sh
+++ scripts/oa-loader.sh
@@ -8,8 +8,8 @@ cd $DATA/openaddresses
 
 # Download New York and New Jersey entries from OpenAddresses
 curl -sS -O http://results.openaddresses.io/state.txt 
-cat state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
-cat state.txt | sed 's/\s\+/\n/g' | grep 'us/nj/statewide' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
+#cat state.txt | sed 's/\s\+/\n/g' | grep 'city_of_new_york' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
+cat state.txt | sed 's/\s\+/\n/g' | grep 'fr/' | grep openaddresses.io | xargs -I {} -n 1 curl -O {}
 ls *.zip | xargs -n 1 unzip -o
 rm *.zip README.*
 
