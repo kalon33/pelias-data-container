9b274e54d7ad85ac46b9774e7315453da029980a
Nicolas Derive <kalon33@ubuntu.com>
2018-01-30 16:26:42 +0100

    Update gtfs-loader.sh

--- scripts/gtfs-loader.sh
+++ scripts/gtfs-loader.sh
@@ -6,19 +6,25 @@ set -e
 mkdir -p $DATA/gtfs
 
 gtfsFiles=(
-    "http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
-    "http://web.mta.info/developers/data/nyct/bus/google_transit_bronx.zip"
-    "http://web.mta.info/developers/data/nyct/bus/google_transit_brooklyn.zip"
-    "http://web.mta.info/developers/data/nyct/bus/google_transit_manhattan.zip"
-    "http://web.mta.info/developers/data/nyct/bus/google_transit_queens.zip"
-    "http://web.mta.info/developers/data/nyct/bus/google_transit_staten_island.zip"
+    #"http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
+    #"http://web.mta.info/developers/data/nyct/bus/google_transit_bronx.zip"
+    #"http://web.mta.info/developers/data/nyct/bus/google_transit_brooklyn.zip"
+    #"http://web.mta.info/developers/data/nyct/bus/google_transit_manhattan.zip"
+    #"http://web.mta.info/developers/data/nyct/bus/google_transit_queens.zip"
+    #"http://web.mta.info/developers/data/nyct/bus/google_transit_staten_island.zip"
     #of course everything is named google_transit.zip
-    "http://web.mta.info/developers/data/lirr/google_transit.zip"
-    "http://web.mta.info/developers/data/mnr/google_transit.zip"
-    "http://web.mta.info/developers/data/busco/google_transit.zip"
-    "http://data.trilliumtransit.com/gtfs/path-nj-us/path-nj-us.zip"
-    "https://dl.dropboxusercontent.com/u/47217822/rail_data(1).zip"
-    "https://dl.dropboxusercontent.com/u/47217822/bus_data(2).zip" 
+    #"http://web.mta.info/developers/data/lirr/google_transit.zip"
+    #"http://web.mta.info/developers/data/mnr/google_transit.zip"
+    #"http://web.mta.info/developers/data/busco/google_transit.zip"
+    #"http://data.trilliumtransit.com/gtfs/path-nj-us/path-nj-us.zip"
+    #"https://dl.dropboxusercontent.com/u/47217822/rail_data(1).zip"
+    #"https://dl.dropboxusercontent.com/u/47217822/bus_data(2).zip"
+    "http://ratp.spiralo.net/stif_gtfs_enhanced_rer_latest.zip"
+    "https://api.idbus.com/gtfs.zip"
+    "http://data.ndovloket.nl/flixbus/flixbus-eu.zip"
+    "https://ressources.data.sncf.com/explore/dataset/sncf-ter-gtfs/files/24e02fa969496e2caa5863a365c66ec2/download/"
+    "https://ressources.data.sncf.com/explore/dataset/sncf-intercites-gtfs/files/ed829c967a0da1252f02baaf684db32c/download/"
+    "http://ratp.spiralo.net/keolis_merged_latest.zip"
 )
 
 # Download gtfs stop data
