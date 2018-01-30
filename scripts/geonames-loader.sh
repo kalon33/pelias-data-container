8aca3251a1d0a6a7983f2f17c8ab8088ba26435a
Nicolas Derive <kalon33@ubuntu.com>
2018-01-30 11:34:07 +0100

    Update geonames-loader.sh

--- scripts/geonames-loader.sh
+++ scripts/geonames-loader.sh
@@ -6,8 +6,8 @@ set -e
 echo '##### downloading US geonames'
 mkdir -p $DATA/geonames
 cd $DATA/geonames
-curl -sS -O http://download.geonames.org/export/dump/US.zip
-unzip -c US.zip US.txt | grep -e 'US[[:space:]]*N[Y|J]' > US.txt
-zip -9u US.zip US.txt
+curl -sS -O http://download.geonames.org/export/dump/FR.zip
+#unzip -c US.zip US.txt | grep -e 'US[[:space:]]*N[Y|J]' > US.txt
+#zip -9u US.zip US.txt
 
 echo 'OK' >> /tmp/loadresults
